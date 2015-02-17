f68b2150afdfb383065c79f3d6e5372f79e48eb4
zachmoshe <zachmoshe@gmail.com>
2015-02-18 01:29:31 +0200

    removing data files (adding to  .gitignore) and adding a script to download them

---
+++ download_datasources.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+curl http://opengeocode.org/cude/download.php?file=/home/fashions/public_html/opengeocode.org/download/cow.txt -o cow.txt
+
+curl http://opengeocode.org/download/worldcities.zip -o worldcities.zip
+unzip worldcities.zip
