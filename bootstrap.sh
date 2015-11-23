29c5f1f92dbca47aa92ed0882d6984448b5728cd
zachmoshe <zachmoshe@gmail.com>
2015-11-23 12:00:47 +0200

    adding readline-devel and running .bash_profile

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 
 # first, install git and clone rbenv and rbenv-build
-sudo yum install git -y
+sudo yum install git readline-devel -y
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+echo '' >> ~/.bash_profile
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
-git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 
 source ~/.bash_profile
 
--- bundler_run.sh
+++ bundler_run.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
+
+# For some reason the HOME env variable is set to '/home/' (while user is 'hadoop')
+# It also seems that something has changed since past versions and .bash_profile isn't run
+HOME=/home/$USER
+. $HOME/.bash_profile
+
 APP_NAME=$1
 EXEC=$2
 shift 2
